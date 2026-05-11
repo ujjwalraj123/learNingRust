137683dee53d5c68970af6aadc64ff37821b8d54
ujjwalraj123 <ujjwalr559@gmail.com>
2026-05-11 03:51:35 -0400

    updated the game comments

--- guessinggame/src/main.rs
+++ guessinggame/src/main.rs
@@ -3,8 +3,8 @@ use rand::Rng;
 fn main() {
     println!("Guessing games begins");
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Please enter your guessed number");
     loop {
-        println!("Please enter your guessed number");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read Input");
         let guess: u32 = match guess.trim().parse() {
@@ -13,14 +13,14 @@ fn main() {
                 continue;
             }
         };
-        println!("Your guessed Number:{guess}");
+        // println!("Your guessed Number:{guess}");
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Try a greater Number"),
             Ordering::Equal => {
                 println!("Congratulations your guessed Number has matched");
                 break;
             }
-            Ordering::Greater => println!("Try a Smaller"),
+            Ordering::Greater => println!("Try a Smaller Number"),
         }
-    }
+    };
 }
